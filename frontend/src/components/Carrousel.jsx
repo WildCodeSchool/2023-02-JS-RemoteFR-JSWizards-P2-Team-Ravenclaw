8f8f6d43e5c16e792eb5d18e79fa9cd3851bd9b9
Samuel Faber <samuel.faber@live.fr>
2023-04-14 09:24:13 +0200

    Ajout des images de test pour le Carrousel

--- frontend/src/components/Carrousel.jsx
+++ frontend/src/components/Carrousel.jsx
@@ -1,9 +1,21 @@
+import demonSlayerSrc from "../assets/images-carrousel/demon-slayer.webp";
+import drStoneSrc from "../assets/images-carrousel/dr-stone.webp";
+import suzumeSrc from "../assets/images-carrousel/suzume.webp";
+
 function Carrousel() {
-    return (
-        <div>
-            Test
-        </div>
-    );
+  return (
+    <div className="carrousel">
+      <div className="carrousel-inner">
+        <img src={demonSlayerSrc} alt="Demon Slayer" />
+      </div>
+      <div className="carrousel-item">
+        <img src={drStoneSrc} alt="Dr. Stone" />
+      </div>
+      <div className="carrousel-item">
+        <img src={suzumeSrc} alt="Suzume" />
+      </div>
+    </div>
+  );
 }
 
 export default Carrousel;
