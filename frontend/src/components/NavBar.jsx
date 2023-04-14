84fce5d12a07472687c6d8de9b2dab9ad445c03c
Samuel Faber <samuel.faber@live.fr>
2023-04-14 15:16:47 +0200

    Modification de la SearchBar dans la NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./NavBar.scss";
-import logo from "../assets/logo/Logo_AlloAnime.png";
 
 export default function NavBar() {
   return (
     <nav>
-      <img src={logo} alt="logo AlloAnime" width="350" height="60" />
-      <input type="text" placeholder="Search ..." name="seach" />
+      <img
+        src="./src/assets/logo/Logo_AlloAnime.png"
+        alt="logo AlloAnime"
+        width="350"
+        height="60"
+      />
+      <label>
+        <input type="text" placeholder="" name="seach" />
+      </label>
       <ul>
         <li>Films</li>
         <li>Series</li>
