2565dea9cb307d53aa7d09119d98cb1549b67901
Alexis Chesneau <[alexischesneau64@gmail.com](mailto:alexischesneau64@gmail.com)>
2023-04-19 09:35:29 +0200

    Réorganisation de la structure HTML dans le fichier NavBar.jsx

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -2,28 +2,34 @@ import React from "react";
 import "./NavBar.scss";
 
 export default function NavBar() {
+  const handleClickBurger = () => {
+    const burger = document.getElementById("burger");
+    const ul = document.querySelector("ul");
+    burger.classList.toggle("open");
+    ul.classList.toggle("open");
+  };
+
   return (
-    <nav>
+    <header>
       <img
-        src="./src/assets/logo/Logo-AlloAnime-desktop.svg"
+        src="./src/assets/logo/Logo_AlloAnime.png"
         alt="logo AlloAnime"
-        className="logo-desktop"
         width="350"
         height="60"
-      />
-      <img
-        src="./src/assets/logo/logo-AlloAnime-mobile.svg"
-        alt="logo AlloAnime"
-        className="logo-mobile"
       />
       <label>
         <input type="text" placeholder="" name="seach" />
       </label>
-      <ul>
-        <li>Films</li>
-        <li>Series</li>
-        <li>Les plus regardes</li>
-      </ul>
-    </nav>
+      <nav>
+        <button type="button" id="link" onClick={handleClickBurger}>
+          <span id="burger" />
+        </button>
+        <ul>
+          <li>Films</li>
+          <li>Series</li>
+          <li>Les plus regardes</li>
+        </ul>
+      </nav>
+    </header>
   );
 }
