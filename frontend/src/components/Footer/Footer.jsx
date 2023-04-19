e77c024eeceb1cb5b2f067df2308990e8030708e
Roxane Belahcene <roxounette@aol.com>
2023-04-19 12:49:01 +0200

    modification du footer jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import "./Footer.scss";
-import logo1 from "../../assets/logo/Facebook.svg";
-import logo2 from "../../assets/logo/Twitter.svg";
-import logo3 from "../../assets/logo/YouTube.svg";
+import logoFacebook from "../../assets/logo/Facebook.svg";
+import logoTwitter from "../../assets/logo/Twitter.svg";
+import logoYoutube from "../../assets/logo/YouTube.svg";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <img src={logo1} alt="logo facebook" width="50" height="50" />
-      <img src={logo2} alt="logo twitter" width="50" height="50" />
-      <img src={logo3} alt="logo youtube" width="50" height="50" />
+      <img
+        src={logoFacebook}
+        className="logo-facebook"
+        alt="logo facebook"
+        width="50"
+        height="50"
+      />
+      <img
+        src={logoTwitter}
+        className="logo-twitter"
+        alt="logo twitter"
+        width="50"
+        height="50"
+      />
+      <img
+        src={logoYoutube}
+        className="logo-youtube"
+        alt="logo youtube"
+        width="50"
+        height="50"
+      />
       <div>
         <p>About Us</p>
         <p>Terms of Us</p>
