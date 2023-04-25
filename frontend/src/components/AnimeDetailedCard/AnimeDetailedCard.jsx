5ba8d4eb4f027edb804bae7dcfaf8cb32c5cbfaa
Samuel Faber <samuel.faber@live.fr>
2023-04-25 11:50:42 +0200

    Test code simple de la AnimeDetailedCard

--- frontend/src/components/AnimeDetailedCard/AnimeDetailedCard.jsx
+++ frontend/src/components/AnimeDetailedCard/AnimeDetailedCard.jsx
@@ -1,1 +1,28 @@
-export default function AnimeDetailedCard() {}
+import PropTypes from "prop-types";
+
+export default function AnimeDetailedCard({ anime }) {
+  return (
+    <div className="anime-card">
+      <img
+        className="image-selection"
+        src={anime.trailer.images.medium_image_url}
+        alt={anime.title}
+      />
+      <h3>{anime.title}</h3>
+      <h4>Synopsis :</h4>
+      <p>{anime.synopsis}</p>
+    </div>
+  );
+}
+
+AnimeDetailedCard.propTypes = {
+  anime: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    trailer: PropTypes.shape({
+      images: PropTypes.shape({
+        medium_image_url: PropTypes.string.isRequired,
+      }).isRequired,
+    }).isRequired,
+    synopsis: PropTypes.string.isRequired,
+  }).isRequired,
+};
