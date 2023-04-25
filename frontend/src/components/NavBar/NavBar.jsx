2acc1a8c4509ce8251184c92edbccde917ce0326
Alexis Chesneau <[alexischesneau64@gmail.com](mailto:alexischesneau64@gmail.com)>
2023-04-25 11:19:49 +0200

    Ajout de fonction onChange onSearch

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import { useState } from "react";
+// import React from "react";
 import "./NavBar.scss";
 
-export default function NavBar() {
+export default function NavBar({ recherche, setRecherche }) {
   const handleClickBurger = () => {
     const burger = document.getElementById("burger");
     const ul = document.querySelector("ul");
     burger.classList.toggle("open");
     ul.classList.toggle("open");
   };
+
+  function onChange(e) {
+    // console.log(e.target.value); /** Cible dans l'input le target puis la valeur */
+    setRecherche(e.target.value);
+    console.log(recherche);
+  }
+
+  function onSearch(e) {
+    e.preventDefault(); /** Evite le rechargement de la page */
+
+    console.log(recherche);
+  }
 
   return (
     <header>
@@ -25,9 +38,17 @@ export default function NavBar() {
         height="30"
         className="logo-mobile"
       />
-      <label>
-        <input type="text" placeholder="" name="seach" />
-      </label>
+      <form onSubmit={onSearch}>
+        <label>
+          <input
+            type="text"
+            placeholder=""
+            name="seach"
+            onChange={onChange}
+            value={recherche}
+          />
+        </label>
+      </form>
       <nav>
         <button type="button" id="link" onClick={handleClickBurger}>
           <span id="burger" />
