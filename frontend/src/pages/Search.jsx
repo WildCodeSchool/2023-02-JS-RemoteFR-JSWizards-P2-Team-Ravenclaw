c1a7f99d77c69e3ea2f8d9f78e1b805004695ede
Alexis Chesneau <[alexischesneau64@gmail.com](mailto:alexischesneau64@gmail.com)>
2023-04-25 11:21:01 +0200

    Création nouvelle page Search.jsx

---
+++ frontend/src/pages/Search.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Footer from "../components/Footer/Footer";
+import NavBar from "../components/NavBar/NavBar";
+
+export default function SearchResult() {
+  const [recherche, setRecherche] = useState("");
+  return (
+    <div>
+      <NavBar recherche={recherche} setRecherche={setRecherche} />
+
+      <Footer />
+    </div>
+  );
+}
