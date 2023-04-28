7f642281f6b1838c4011f9fb1825f86a0b46d742
Samuel Faber <samuel.faber@live.fr>
2023-04-28 10:29:03 +0200

    Fix SCSS de Footer et Contact

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -1,28 +1,26 @@
 import React from "react";
-import NavBar from "../../components/NavBar/NavBar";
-import Footer from "../../components/Footer/Footer";
 import "./Contact.scss";
 
 export default function Contact() {
   return (
-    <div>
-      <NavBar />
-      <form>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input id="name" type="name" />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input id="email" type="text" />
-        </div>
-        <div>
-          <label htmlFor="message">Message</label>
-          <input id="message" type="message" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-      <Footer />
-    </div>
+    <form className="contact-form">
+      <div>
+        <label htmlFor="name">Name</label>
+        <input id="name" type="name" />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="text" />
+      </div>
+      <div>
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          type="message"
+          placeholder="Saisissez votre message ici"
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
