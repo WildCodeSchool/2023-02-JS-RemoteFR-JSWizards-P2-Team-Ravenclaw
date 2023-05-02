e14fecbe8e5d6af6b1400cf60f944a2246c5e026
Samuel Faber <samuel.faber@live.fr>
2023-05-02 16:41:53 +0200

    Sous-composant 'Episode' avec son CSS associé

---
+++ frontend/src/pages/Anime/Episode/Episode.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import "./Episode.scss";
+
+export default function Episode({ episode }) {
+  return (
+    <div className="episode">
+      <img src={episode.images.jpg.image_url} alt={episode.title} />
+      <h3>{episode.episode}</h3>
+      <h4>{episode.title}</h4>
+    </div>
+  );
+}
+
+Episode.propTypes = {
+  episode: PropTypes.shape({
+    episode: PropTypes.string,
+    title: PropTypes.string,
+    images: PropTypes.shape({
+      jpg: PropTypes.shape({
+        image_url: PropTypes.string,
+      }),
+    }),
+  }).isRequired,
+};
