418916766469b012771f3d9fa485a9186cf933b4
Samuel Faber <samuel.faber@live.fr>
2023-05-02 17:03:23 +0200

    Ajout d'un 'Link' sur les AnimeDetailedCards

--- frontend/src/components/AnimeDetailedCard/AnimeDetailedCard.jsx
+++ frontend/src/components/AnimeDetailedCard/AnimeDetailedCard.jsx
@@ -1,19 +1,23 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import "./AnimeDetailedCard.scss";
 
 export default function AnimeDetailedCard({ anime }) {
   return (
     <div className="anime-detailed-card">
-      <img src={anime.images.jpg.large_image_url} alt={anime.title} />
-      <h3>{anime.title}</h3>
-      <h4>Synopsis :</h4>
-      <p>{anime.synopsis}</p>
+      <Link to={`/anime/${anime.mal_id}`}>
+        <img src={anime.images.jpg.large_image_url} alt={anime.title} />
+        <h3>{anime.title}</h3>
+        <h4>Synopsis :</h4>
+        <p>{anime.synopsis}</p>
+      </Link>
     </div>
   );
 }
 
 AnimeDetailedCard.propTypes = {
   anime: PropTypes.shape({
+    mal_id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     images: PropTypes.shape({
       jpg: PropTypes.shape({
