19977f3e74310def83f67b42a1a647f30eed9f86
Badis Beloucif <beloucifbadis@gmail.com>
2023-05-03 15:37:21 +0200

    modification taille images AnimeDetailedCard et  mise en forme du CSS

--- frontend/src/components/AnimeDetailedCard/AnimeDetailedCard.jsx
+++ frontend/src/components/AnimeDetailedCard/AnimeDetailedCard.jsx
@@ -4,14 +4,10 @@ import "./AnimeDetailedCard.scss";
 export default function AnimeDetailedCard({ anime }) {
   return (
     <div className="anime-detailed-card">
-      <div className="card-left">
-        <img src={anime.images.jpg.large_image_url} alt={anime.title} />
-        <h3>{anime.title}</h3>
-      </div>
-      <div className="card-right">
-        <h4>Synopsis :</h4>
-        <p>{anime.synopsis}</p>
-      </div>
+      <img src={anime.images.jpg.large_image_url} alt={anime.title} />
+      <h3>{anime.title}</h3>
+      <h4>Synopsis :</h4>
+      <p>{anime.synopsis}</p>
     </div>
   );
 }
