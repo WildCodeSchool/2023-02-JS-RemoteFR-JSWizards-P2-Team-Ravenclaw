df2487ad7a8b62bc98aa55e2f4b99ea47a627c6a
Samuel Faber <samuel.faber@live.fr>
2023-05-04 14:17:13 +0200

    Modification des liens API de certaines pages de catégorie

--- frontend/src/pages/FavorisPage.jsx
+++ frontend/src/pages/FavorisPage.jsx
@@ -3,7 +3,7 @@ import SearchResult from "../components/SearchResult/SearchResult";
 
 function FavorisPage() {
   const url =
-    "https://api.jikan.moe/v4/anime?type=tv&min_score=8&order_by=rank&limit=25";
+    "https://api.jikan.moe/v4/anime?type=tv&min_score=8&order_by=rank";
   return <SearchResult url={url} />;
 }
 
